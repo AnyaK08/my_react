ae85cab8014b400bdc0bc87c128b9a499eb171e3
AnyaK08 <kozina.anya08@gmail.com>
2024-09-24 18:29:09 +0200

    +child to parent

--- practice 8.09(components comuncation)/my-app/src/App.js
+++ practice 8.09(components comuncation)/my-app/src/App.js
@@ -4,28 +4,28 @@ import Child from "./child/Child";
 import { useState, useRef } from "react";
 
 function App() {
-  const [parentTxt, setParentTxt] = useState("start");
+  const [parentTxt, setParentTxt] = useState("parent start value");
   const inputTxt = useRef(null);
+
   function newTxt() {
     console.log(inputTxt.current.value);
     setParentTxt(inputTxt.current.value);
   }
-  function setNewTxt() {
-        console.log(inputTxt.current.value);
-    inputTxt.current.value = "new";
-        console.log(inputTxt.current.value);
+
+  function setTxtFromChild(newTxt) {
+    inputTxt.current.value = newTxt;
   }
 
   return (
     <div className="App text-center container p-4 ">
       <h2 className="mb-4">Parent</h2>
       {/* <input type="text" className="form-control mb-4" ref={inputTxt} onChange={newTxt}></input> */}
-      <input type="text" className="form-control mb-4" ref={inputTxt} ></input>
-      <button id="btn_parent" className="btn btn-primary mb-4" onClick={setNewTxt}>
+      <input type="text" className="form-control mb-4" ref={inputTxt}></input>
+      <button id="btn_parent" className="btn btn-primary mb-4" onClick={newTxt}>
         Send to Child
       </button>
-
-      <Child txt={parentTxt}></Child>
+{/* @TODO  send object as props */}
+      <Child txt={parentTxt} fnc={setTxtFromChild}></Child>
     </div>
   );
 }
--- practice 8.09(components comuncation)/my-app/src/child/Child.js
+++ practice 8.09(components comuncation)/my-app/src/child/Child.js
@@ -1,14 +1,32 @@
 import './Child.css';
+import { useRef, useEffect } from "react";
 
 export default function Child(props) {
     const txt = props.txt;
-
-
+    const setTxtFromChild = props.fnc;
+    const childTxt = useRef(null);
+    function sentToParent() {
+        setTxtFromChild(childTxt.current.value);
+    }
+    useEffect(() => {
+        childTxt.current.value = "start value"
+    }, []);
+     useEffect(() => {
+       childTxt.current.value = txt;
+     }, [txt]);
+    
     return (
       <div className="Child text-center container ">
         <h2 className="mb-4">Child</h2>
-        <input type="text" className="form-control mb-4" value={txt}></input>
-        <button id="btn_parent" className="btn btn-primary mb-4">
+        <input
+          type="text"
+          className="form-control mb-4"
+      
+          ref={childTxt}
+        ></input>
+            <button id="btn_parent"
+                onClick= {sentToParent}
+                className="btn btn-primary mb-4">
           Send to Parent
         </button>
       </div>
